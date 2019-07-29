62e276bf62d43b5ae1d51da1ce507db6b6c44662
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 05:26:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,9 +34,9 @@ class ApplicationController < Sinatra::Base
 
   post "/recipes/:id" do
     @recipe = Recipe.find(params[:id])
-    @recipe.name = params[:name] if !!params[:name]
-    @recipe.ingredients = params[:ingredients] if !!params[:ingredients]
-    @recipe.cook_time = params[:cook_time] if !!params[:cook_time]
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
     @recipe.save
 
     redirect "recipes/#{params[:id]}"
