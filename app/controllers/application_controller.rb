c382f1ea47c0704b1d4a32af923403cfcbcc0102
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-29 04:18:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,6 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients] if !!params[:ingredients]
     @recipe.cook_time = params[:cook_time] if !!params[:cook_time]
 
+    erb :recipe
   end
 end
